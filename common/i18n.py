b9d350b0238c04dd3367fa69c28d5d6e9f013e99
eFini <ricklan@gmail.com>
2023-01-31 15:14:54 +0800

    Update i18n.py
    Added missing language.

--- common/i18n.py
+++ common/i18n.py
@@ -4,9 +4,13 @@ from common.params import Params
 locale_dir = "/data/openpilot/selfdrive/assets/locales"
 # supported_language = ["en-US", "zh-TW", "zh-CN", "ja-JP", "ko-KR"]
 supported_languages = {
+  "main_en": "en-US",
   "main_zh-CHT": "zh-TW",
   "main_zh-CHS": "zh-CN",
   "main_ko": "ko-KR",
+  "main_ja": "ja-JP",
+  "main_de": "de-DE",
+  "main_pt-BR": "pt_BR",
 }
 
 def events():
