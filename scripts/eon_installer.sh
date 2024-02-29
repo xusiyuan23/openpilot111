a7808e2ee4db5e9e6b2e4901acea33b907d0b61f
eFini <ricklan@gmail.com>
2024-02-29 16:33:17 +0800

    Update eon_installer.sh

--- scripts/eon_installer.sh
+++ scripts/eon_installer.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
-rm -fr /data/openpilot
-cd /data/ && git clone https://github.com/dragonpilot-community/dragonpilot -b $1 --single-branch --depth 1 openpilot
+rm -fr /data/openpilot ;
+cd /data/ &&
 
-touch /data/data/com.termux/files/continue.sh
-echo "#!/usr/bin/bash" > /data/data/com.termux/files/continue.sh
-echo "cd /data/openpilot" >> /data/data/com.termux/files/continue.sh
-echo "exec ./launch_openpilot.sh" >> /data/data/com.termux/files/continue.sh
+git clone https://github.com/dragonpilot-community/dragonpilot -b $1 --single-branch -depth=1 openpilot &&
 
-chmod u+x /data/data/com.termux/files/continue.sh
+touch /data/data/com.termux/files/continue.sh &&
+echo "#!/usr/bin/bash" >> /data/data/com.termux/files/continue.sh &&
+echo "cd /data/openpilot" >> /data/data/com.termux/files/continue.sh &&
+echo "exec ./launch_openpilot.sh" >> /data/data/com.termux/files/continue.sh &&
+
+chmod u+x /data/data/com.termux/files/continue.sh &&
 reboot
