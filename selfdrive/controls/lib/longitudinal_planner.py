f6095ef8e23700a2d0d405fcb4f23afd8b06fb2b
Rick Lan <ricklan@gmail.com>
2024-06-13 19:59:17 +0800

    Disable use of navInstruction for now

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -93,7 +93,12 @@ class LongitudinalPlanner:
 
     if self._dynamic_endtoend_controller.is_enabled():
       self._dynamic_endtoend_controller.set_mpc_fcw_crash_cnt(self.mpc.crash_cnt)
-      self.mpc.mode = self._dynamic_endtoend_controller.get_mpc_mode(self.CP.radarUnavailable, sm['carState'], sm['radarState'].leadOne, sm['modelV2'], sm['controlsState'], sm['navInstruction'].maneuverDistance)
+      self.mpc.mode = self._dynamic_endtoend_controller.get_mpc_mode(self.CP.radarUnavailable,
+                                                                     sm['carState'],
+                                                                     sm['radarState'].leadOne,
+                                                                     sm['modelV2'],
+                                                                     sm['controlsState']
+                                                                     ) #, sm['navInstruction'].maneuverDistance)
     else:
       self.mpc.mode = 'blended' if sm['controlsState'].experimentalMode else 'acc'
 
